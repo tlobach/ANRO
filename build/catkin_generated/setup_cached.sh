c07cf3ad031c8711b13063ebbf2ef2ba6df06eb5
tlobach <toni9619.al@gmail.com>
2017-03-26 23:46:15 +0200

    ANRO_LAB1

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/alobach/catkin_ws/src/anro/build/devel:$CMAKE_PREFIX_PATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/alobach/catkin_ws/src/anro/build/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/alobach/catkin_ws/src/anro:$ROS_PACKAGE_PATH"
---
+++ build/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/gtest
+# Build directory: /home/alobach/catkin_ws/src/anro/build/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
